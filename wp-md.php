da18ab2139638d7dac68c1269aefff6b0ee615a3
Loic Blot <loic.blot@unix-experience.fr>
2017-10-14 13:18:16 +0200

    Init project

---
+++ wp-md.php
@@ -0,0 +1,13 @@
+<?php
+   /*
+   Plugin Name: Wordpress Markdown
+   Plugin URI: https://github.com/nerzhul/wp-md
+   Description: a plugin to use markdown in articles
+   Version: 0.1
+   Author: Loic Blot
+   Author URI: https://www.unix-experience.fr
+   License: GPL3
+   */
+
+   require_once __DIR__ . '/vendor/autoload.php';
+?>
