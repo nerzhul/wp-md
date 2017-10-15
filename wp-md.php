8748ba3c921a993cac873c52ed87e5a33ef58894
Loic Blot <loic.blot@unix-experience.fr>
2017-10-15 10:32:15 +0200

    Bump version to 1.0

--- wp-md.php
+++ wp-md.php
@@ -3,7 +3,7 @@
     Plugin Name: Wordpress Markdown
     Plugin URI: https://github.com/nerzhul/wp-md
     Description: a plugin to use markdown in articles
-    Version: 0.1
+    Version: 1.0
     Author: Loic Blot
     Author URI: https://www.unix-experience.fr
     License: GPL3
