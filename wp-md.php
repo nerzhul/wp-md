b1e5a74d0c794ba6ae65d6384c702704bc73208c
Loic Blot <loic.blot@unix-experience.fr>
2017-10-15 10:14:23 +0200

    Prepare parsedown & Wordpress shortcodes

--- wp-md.php
+++ wp-md.php
@@ -1,13 +1,29 @@
 <?php
-   /*
-   Plugin Name: Wordpress Markdown
-   Plugin URI: https://github.com/nerzhul/wp-md
-   Description: a plugin to use markdown in articles
-   Version: 0.1
-   Author: Loic Blot
-   Author URI: https://www.unix-experience.fr
-   License: GPL3
-   */
+    /*
+    Plugin Name: Wordpress Markdown
+    Plugin URI: https://github.com/nerzhul/wp-md
+    Description: a plugin to use markdown in articles
+    Version: 0.1
+    Author: Loic Blot
+    Author URI: https://www.unix-experience.fr
+    License: GPL3
+    */
 
-   require_once __DIR__ . '/vendor/autoload.php';
+    require_once __DIR__ . '/vendor/autoload.php';
+
+    function WordpressMarkdownShortCode($params = array()) {
+        extract(shortcode_atts(array(
+            'md_file' => null,
+        ), $params));
+
+        if ($md_file === null) {
+            return "Unknown markdown file";
+        }
+
+        $Parsedown = new Parsedown();
+
+        return $Parsedown->text('Hello _Parsedown_!');
+    }
+
+    add_shortcode('wp-md', 'WordpressMarkdownShortCode');
 ?>
